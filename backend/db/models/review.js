8de30bb3fe1d5710f5c0ccb199ecfc0732d4a0f8
justin <justin_shim89@yahoo.com>
2023-06-15 00:27:09 -0500

    m and s

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -40,7 +40,7 @@ module.exports = (sequelize, DataTypes) => {
       onDelete: "CASCADE"
     },
     review: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false,
     },
     stars: {
