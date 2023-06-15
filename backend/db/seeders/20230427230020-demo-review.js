a24d7e4a32b2e0a0afc7ac61975c652e75dbad3b
justin <justin_shim89@yahoo.com>
2023-06-15 00:33:46 -0500

    m and s

--- backend/db/seeders/20230427230020-demo-review.js
+++ backend/db/seeders/20230427230020-demo-review.js
@@ -15,19 +15,19 @@ module.exports = {
       {
         userId: 1,
         spotId: 1,
-        review: "This AirBnB was worth every penny! The view from the balcony was breathtaking and the interior design was top-notch. The amenities provided were luxurious and made our stay extremely comfortable. The host was also very accommodating and responsive to our needs. Highly recommend!",
+        review: "This AirBnB was worth every penny!",
         stars: 4,
       },
       {
         userId: 2,
         spotId: 2,
-        review: "I've stayed at many AirBnBs but this one takes the cake! The attention to detail in every aspect of this rental was impressive. From the high-end appliances to the plush bedding, no expense was spared. The location was also unbeatable, right in the heart of the city with stunning views. Will definitely be returning!",
+        review: "I've stayed at many AirBnBs but this one takes the cake!",
         stars: 5,
       },
       {
         userId: 3,
         spotId: 3,
-        review: "If you're looking for a luxurious getaway, this AirBnB is the perfect choice. The modern and stylish design of the interior was matched by the stunning views from every window. The host was incredibly welcoming and made sure every detail was taken care of. It was truly a five-star experience.",
+        review: "If you're looking for a luxurious getaway, this AirBnB is the perfect choice.",
         stars: 3,
       }
     ], {});
