3e40b866a5ce57242c381a7fe0d45e7e41667537
justin <justin_shim89@yahoo.com>
2023-06-19 14:52:46 -0500

    authorization for reviewimages and spotimages

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -8,11 +8,17 @@ const { setTokenCookie, requireAuth, restoreUser } = require("../../utils/auth")
 router.delete('/:reviewImageId',
     requireAuth,
     async (req, res) => {
-    let deletedImage = await ReviewImage.findByPk(req.params.reviewImageId);
-    if (!deletedImage) res.json({ message: "Review Image couldn't be found", statusCode: 404 });
-    await deletedImage.destroy();
+        // Authorization
+        let findUser = await User.findByPk(req.user.id);
+        const findSpot = await Spot.findByPk(req.params.reviewImageId);
+        findUser = findUser.toJSON();
+        if (findUser.id !== findSpot.ownerId) res.json({ message: "Forbidden", statusCode: 403 });
 
-    res.json({ message: "Successfully deleted", statusCode: 200 })
-});
+        let deletedImage = await ReviewImage.findByPk(req.params.reviewImageId);
+        if (!deletedImage) res.json({ message: "Review Image couldn't be found", statusCode: 404 });
+        await deletedImage.destroy();
+
+        res.json({ message: "Successfully deleted", statusCode: 200 })
+    });
 
 module.exports = router;
--- backend/routes/api/spots-images.js
+++ backend/routes/api/spots-images.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const { handleValidationErrors } = require('../../utils/validation');
-const { User, Spot, SpotsImage, ReviewImage, Booking, Review, Sequelize } = require('../../db/models');
+const { User, Spot, SpotsImage, ReviewImage, Booking, Review } = require('../../db/models');
 const { check, param } = require("express-validator");
 const { setTokenCookie, requireAuth, restoreUser } = require("../../utils/auth");
+const { Sequelize } = require("sequelize");
 
 router.delete('/:spotImageId',
     requireAuth,
     async (req, res) => {
-    let deletedImage = await SpotsImage.findByPk(req.params.spotImageId);
-    if (!deletedImage) res.json({ message: "Spot Image couldn't be found", statusCode: 404 });
-    await deletedImage.destroy();
+        // Authorization
+        let findUser = await User.findByPk(req.user.id);
+        const findSpot = await Spot.findByPk(req.params.spotImageId);
+        findUser = findUser.toJSON();
+        if (findUser.id !== findSpot.ownerId) res.json({ message: "Forbidden", statusCode: 403 });
 
-    res.json({ message: "Successfully deleted", statusCode: 200 })
-});
+        let deletedImage = await SpotsImage.findByPk(req.params.spotImageId);
+        if (!deletedImage) res.json({ message: "Spot Image couldn't be found", statusCode: 404 });
+        await deletedImage.destroy();
+
+        res.json({ message: "Successfully deleted", statusCode: 200 })
+    });
 
 module.exports = router;
